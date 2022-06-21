397e2660174f55ba86c47c1fd5d80f9c7cd5780b
joel <joelkalema63@gmail.com>
2022-06-21 19:23:04 +0200

    🗃️ add class

--- teacher.rb
+++ teacher.rb
@@ -1,10 +1,12 @@
 require "./person.rb"
 
-def initialize(age, name = 'Unknown', parent_permission = true, specialization)
-  super(age, name, parent_permission)
-  @specialization = specialization
-end
-
-def can_use_services?
-  true
+class Teacher < Person
+  def initialize(age, name = 'Unknown', parent_permission = true, specialization)
+    super(age, name, parent_permission)
+    @specialization = specialization
+  end
+  
+  def can_use_services?
+    true
+  end
 end
