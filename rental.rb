1e14b6acd23adad5bd6460691797ec1d0ff53a65
joel <joelkalema63@gmail.com>
2022-06-25 03:12:31 +0200

    💥 fix problem

--- rental.rb
+++ rental.rb
@@ -1,11 +1,12 @@
 class Rental
-  attr_accessor :date, :person, :book
+  attr_accessor :date
+  attr_reader :book, :person
 
   def initialize(date:, person:, book:)
     @date = date
     @person = person
-    person.add_rental(self)
     @book = book
     book.add_rental(self)
   end
 end
+
